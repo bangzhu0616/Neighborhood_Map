ca2d4c5f1357b8238dba81376253c1dd9b79e996
xiaofeng <haoxiaofeng19@gmail.com>
2015-05-25 22:36:18 -0600

    submitted to review

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Front-End-P5
-Udacity front-end P5
+This page is going to search 10 top places in fousquare locations database via foursquare api based on user's input.
+
+# How to start
+1. Open index.html in a web browser.
+2. User could input three key words for searching, then click the search button.
+	City: 		The city user want search
+	State: 		The state of this city
+	Section:	What kind of locations does user want? User could input one of food, drinks, coffee, shops, arts, outdoors, sights, trending or specials, nextVenues (venues frequently visited after a given venue), or topPicks (a mix of recommendations generated without a query from the user). 
+
+# What will show
+On the left side, a google map with ten markers will be shown if user's input is legal. On the right side, a list of this ten places will be shown. 
--- js/app.js
+++ js/app.js
@@ -1,15 +1,22 @@
-
+// locations_found: an array of 10 locations found in foursquare database based on user's input
+// Model
 var locations_found =[];
+// url0: use foursquare explore api
 var url0 = 'https://api.foursquare.com/v2/venues/explore?';
 var oauth = '&limit=10&oauth_token=HUQKWZ2KCYLF52D3ZZ1JTVLGML2WVYMCPBSE0LERJZBL5AIC&v=20150119';
 
+// Add locations as markers on a google map
+// View
 var addMarkers = function(){
 	var self = this;
+	// create a google map
 	var map = new google.maps.Map(document.getElementById('map-canvas'), {
 				zoom: 4,
 				center: google.maps.LatLng(40.766511,-111.842214)
 			});
+	// set the bound of this google map
 	this.bounds = new google.maps.LatLngBounds();
+	// for each location set a marker
 	locations_found.forEach(function(loc){
 		var myLatlng = new google.maps.LatLng(loc.lat, loc.lng);
 		var marker = new google.maps.Marker({
@@ -18,6 +25,7 @@ var addMarkers = function(){
 			title: loc.name
 		});
 		var infowindow = null;
+		// when click a marker, pop a window to show some information
 		google.maps.event.addListener(marker, 'click', function(){
 			infowindow = new google.maps.InfoWindow({
 				content: loc.content
@@ -26,16 +34,21 @@ var addMarkers = function(){
 		});
 		self.bounds.extend(myLatlng);
 	});
+	// fit the bound
 	map.fitBounds(this.bounds);
 };
 
+// add foursquare locations in fourlist and render them to the page
+// View
 var addFours = function(pre){
 	pre.fourlist(locations_found);
 };
 
+//ViewModel
 var ViewModel = function() {
 	var self = this;
 
+	// all the variables connect js file and html file
 	this.cityInput = ko.observable('');
 	this.stateInput = ko.observable('');
 	this.sectionInput = ko.observable('');
@@ -43,19 +56,23 @@ var ViewModel = function() {
 	this.fourlist = ko.observableArray([]);
 	this.greeting = ko.observable('');
 
+	// start search
 	this.keysearch = function() {
+		// get user's input
 		var city = self.cityInput();
 		var state  = self.stateInput();
 		var section = self.sectionInput();
+		// generate the foursquare api url
 		var url = url0+'near='+city+','+state+'&section='+section+oauth;
 		
+		// clear the last search data
 		locations_found = [];
 		self.fourhead('');
 		self.fourlist([]);
 
+		// get json data from foursquare api
 		$.getJSON(url, function(data){
 			self.greeting('Found places for you');
-			//self.fourhead('foursquare '+section+' found in '+city+' '+state);
 			var places = data.response.groups[0].items;
 			for (var i=0; i<places.length; i++) {
 				var place = places[i];
@@ -73,7 +90,7 @@ var ViewModel = function() {
 
 			addMarkers();
 			addFours(self);
-
+		// render an error message when can not get json data
 		}).error(function(){
 			self.greeting('Can not find places for you. Input error!');
 			$('#map-canvas').html('');
